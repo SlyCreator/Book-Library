5e6899ad4c8f90c82ddeb059ce1f25e4246bd777
slycreator <chinonso1amaechi@gmial.com>
2021-11-12 23:42:56 +0100

    setup

---
+++ src/main/java/com/slycreator/booklibraryassessment/BookLibraryAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.slycreator.booklibraryassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookLibraryAssessmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookLibraryAssessmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/slycreator/booklibraryassessment/BookLibraryAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slycreator.booklibraryassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookLibraryAssessmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
